5279c77f927e4a12dcb92faacb8ede1cb7b411de
Ekaterina Scherbakova <katarin08@gmail.com>
2019-09-23 23:31:15 +0300

    добавлена статистика

---
+++ js/stat.js
@@ -0,0 +1,34 @@
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GRAPH_HEIGHT = 150;
+var GRAPH_STEP = 90;
+
+window.renderStatistics = function(ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(CLOUD_X + 10, CLOUD_Y + 10, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000';
+  ctx.fillText('Ура вы победили!', CLOUD_X + 30, CLOUD_Y + 20);
+  ctx.fillText('Список результатов:', CLOUD_X + 30, CLOUD_Y + 40);
+
+  var maxTime = Math.max(...times);
+
+  for (var i = 0; i < times.length; i++){
+    var scoreHeight = GRAPH_HEIGHT * times[i] / maxTime;
+    var scorePositionX = CLOUD_X + 50 + GRAPH_STEP * i;
+
+    ctx.fillStyle = (names[i] === 'Вы')
+      ? 'rgba(255, 0, 0, 1)'
+      : 'hsl(240,' + Math.ceil(Math.random() * 100) + '% , 50%)';
+    ctx.fillRect(scorePositionX, CLOUD_HEIGHT - 25, 40, -scoreHeight);
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.ceil(times[i]), scorePositionX, (CLOUD_HEIGHT - 25 - scoreHeight - 20));
+    ctx.fillText(names[i], scorePositionX, CLOUD_HEIGHT - 15);
+  }
+}
