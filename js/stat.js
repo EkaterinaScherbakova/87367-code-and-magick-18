a92dea2b3e9484dab9c0668946e7f8f68570fcf9
Ekaterina Scherbakova <katarin08@gmail.com>
2019-09-25 12:01:01 +0300

    refactoring

--- js/stat.js
+++ js/stat.js
@@ -7,18 +7,22 @@ var CLOUD_Y = 10;
 var GRAPH_HEIGHT = 150;
 var GRAPH_STEP = 90;
 
-window.renderStatistics = function (ctx, names, times) {
+var createCloud = function (ctx) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(CLOUD_X + 10, CLOUD_Y + 10, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = '#fff';
   ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
+var createCloudTitle = function (ctx) {
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillStyle = '#000';
   ctx.fillText('Ура вы победили!', CLOUD_X + 30, CLOUD_Y + 20);
   ctx.fillText('Список результатов:', CLOUD_X + 30, CLOUD_Y + 40);
+};
 
+var createStatistic = function (ctx, names, times) {
   var maxTime = Math.max.apply(Math, times);
 
   for (var i = 0; i < times.length; i++) {
@@ -33,4 +37,10 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText(Math.ceil(times[i]), scorePositionX, (CLOUD_HEIGHT - 25 - scoreHeight - 20));
     ctx.fillText(names[i], scorePositionX, CLOUD_HEIGHT - 15);
   }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  createCloud(ctx);
+  createCloudTitle(ctx);
+  createStatistic(ctx, names, times);
 };
