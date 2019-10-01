46f20cec8fe1098446929e9c3d39fcc74aa2273c
Ekaterina Scherbakova <katarin08@gmail.com>
2019-10-01 16:39:09 +0300

    добавлен выбор персонажа

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COATCOLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -25,5 +22,29 @@ var generateCharacters = function () {
   return characters;
 };
 
-var characters = generateCharacters();
-window.console.log(characters);
+var createWizard = function (character) {
+  var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  var element = template.cloneNode(true);
+  var label = element.querySelector('.setup-similar-label');
+  var coatColor = element.querySelector('.wizard-coat');
+  var eyesColor = element.querySelector('.wizard-eyes');
+
+  label.textContent = character.name;
+  coatColor.style.fill = character.coatColor;
+  eyesColor.style.fill = character.eyesColor;
+  return element;
+};
+
+var appendWizards = function (characters) {
+  var list = document.querySelector('.setup-similar-list');
+
+  for (var i = 0; i < CHARACTERS_COUNT; i++) {
+    var element = createWizard(characters[i]);
+    list.appendChild(element);
+  }
+};
+
+
+document.querySelector('.setup').classList.remove('hidden');
+appendWizards(generateCharacters());
+document.querySelector('.setup-similar').classList.remove('hidden');
